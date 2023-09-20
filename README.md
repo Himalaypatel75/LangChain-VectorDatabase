cbb47ec9c38993ca6f9ffaad19bde9a8be9c957c
himalay patel <himalaypatel75@gmail.com>
2023-09-20 17:14:37 +0530

    text and pdf read completed

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # LangChain-VectorDatabase
 this is contains code for vector database in langchain
+
+reading pdf and text files in it.
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from langchain.vectorstores import FAISS, Pinecone
 from langchain.chains import RetrievalQA, VectorDBQA
 from langchain.llms import OpenAI
 import os
-import pinecone
+import pinecone  # to store vector data we use pinecone
 from dotenv import load_dotenv
 
 load_dotenv()  # this will give variables from .env file
@@ -18,21 +18,57 @@ pinecone.init(
 
 if __name__ == "__main__":
     print("Hello World!")
-    loader = TextLoader(os.getenv("FILE_PATH"))
-    document = loader.load()
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    texts = text_splitter.split_documents(documents=document)
-    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 
-    docsearch = Pinecone.from_documents(
-        texts, embedding=embeddings, index_name=os.getenv("PINECONE_INDEX_NAME")
-    )
+    read = "pdf"
 
-    qa = VectorDBQA.from_chain_type(
-        llm=OpenAI(), chain_type="stuff", vectorstore = docsearch, return_source_documents = True
-    )
-    
-    query = "What is a vector DB? Give me a 15 word answer for a beginner"
-    
-    result = qa({"query" : query})
-    print(result)
+    if read == "text":
+        # ---------reading txt with pinecone------------
+        loader = TextLoader(os.getenv("FILE_PATH"))
+        document = loader.load()
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        texts = text_splitter.split_documents(documents=document)
+        embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+
+        docsearch = Pinecone.from_documents(
+            texts, embedding=embeddings, index_name=os.getenv("PINECONE_INDEX_NAME")
+        )
+
+        qa = VectorDBQA.from_chain_type(
+            llm=OpenAI(),
+            chain_type="stuff",
+            vectorstore=docsearch,
+            return_source_documents=True,
+        )
+
+        query = "What is a vector DB? Give me a 15 word answer for a beginner"
+
+        result = qa({"query": query})
+        print(result)
+        # ---------reading txt with pinecone------------
+
+    elif read == "pdf":
+        # ---------reading txt with FAISS------------
+
+        pdf_path = os.getenv("PDF_FILE_PATH")
+
+        loader = PyPDFLoader(file_path=pdf_path)
+        documents = loader.load()
+        text_splitter = CharacterTextSplitter(
+            chunk_size=1000, chunk_overlap=30, separator="\n"
+        )
+        docs = text_splitter.split_documents(documents=documents)
+
+        embeddings = OpenAIEmbeddings()
+        vectorstore = FAISS.from_documents(docs, embeddings)
+        vectorstore.save_local("faiss_index_book")
+
+        new_vectorstore = FAISS.load_local("faiss_index_book", embeddings)
+        
+        qa = RetrievalQA.from_chain_type(
+            llm=OpenAI(), chain_type="stuff", retriever=new_vectorstore.as_retriever()
+        )
+        res = qa.run("what is three mistakes he made?")
+        print(res)
+
+        # ---------reading txt with FAISS------------
+        pass
